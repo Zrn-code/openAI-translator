f0d7878ab51249382f7a3d2a698ba69c384be439
Zrn_code <zxc0978827909@gmail.com>
2023-06-14 15:12:36 +0800

    add image for mainPage display

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -13,10 +13,11 @@ function MainPage() {
   };
 	
   return (
-		<div className="flex w-full lg:flex-row toast toast-center toast-middle">
-			<div className="card lg:card-side bg-base-100 shadow-xl basis-2/3">
+		<div className="flex max-h-full h-3/5 w-full lg:flex-row toast toast-center toast-middle">
+			<div className="card lg:card-side bg-base-100 shadow-xl basis-2/3 image-full">
+				<figure className='blur-sm'><img src="/images/translator.jpg" alt="Album"/></figure>
 				<div className="card-body">
-					<h2 className="card-title">{t(`navbar.${MAIN_ITEMS[selected].label}`)}</h2>
+					<h2 className="card-title text-2xl">{t(`navbar.${MAIN_ITEMS[selected].label}`)}</h2>
 					<p>Describe the Main Function.</p>
 					<div className="card-actions justify-end">
 						<Link to={MAIN_ITEMS[selected].to}className="btn btn-primary">{t('Go')}</Link>
@@ -24,12 +25,12 @@ function MainPage() {
 				</div>
 			</div>
 			<div className="divider divider-horizontal" />
-			<div className="flex flex-row basis-1/4">
+			<div className="flex flex-row basis-1/4 space-x-4">
 				{ MAIN_ITEMS.map(({to,icon,label,index}) => (
 					<div 
 						key={index} 
 						onClick={() => handleClick(index)} 
-						className={clsx("flex flex-wrap basis-1/2 place-items-center btn",selected === index?"btn-primary":"bg-base-300")}>{icon}{t(`navbar.${label}`)}
+						className={clsx("flex flex-wrap basis-4/10 place-items-center btn h-15",selected === index?"btn-primary":"bg-base-300")}>{icon}{t(`navbar.${label}`)}
 					</div>))
 				}
 				</div>
